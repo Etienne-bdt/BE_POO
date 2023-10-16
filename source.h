ae8df7bd6e2d49374c3cdedf534e23013f4f72a2
Florian <florian.escaffre@gmail.com>
2023-10-16 17:09:53 +0200

    feat: completion du header

--- source.h
+++ source.h
@@ -10,6 +10,36 @@ public:
     virtual float ve(float t);
 };
 
+class source_periodique: public source {
+    source_periodique();
+};
 
+class source_aperiodique: public source {
+    source_aperiodique();
+};
 
+class source_sin: public source_periodique {
+    public:
+        source_sin(float t, float a, float p);
+};
+
+class source_triangulaire : public source_periodique{
+    public:
+        source_triangulaire(float t, float a, float p);
+};
+
+class creneau : public source_periodique{
+    public:
+        creneau(float t, float a, float p);
+};
+
+class source_rectangulaire: public source_aperiodique{
+    public:
+        source_rectangulaire(float t, float a, float p);
+};
+
+class echelon: public source_aperiodique{
+    public:
+        echelon(float t, float a, float p);
+};
 
