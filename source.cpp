08595f2644954da57688150d5fe24c622b1b7d02
Etienne Bardet <bardete@bde.enseeiht.fr>
2023-10-16 17:07:32 +0200

    feat: sources et correction de l'heritage

--- source.cpp
+++ source.cpp
@@ -17,32 +17,34 @@ class source {
         float phase;
 };
 
-class source_periodique: source {
+class source_periodique: public source {
     
     protected:
         float periode;
 };
 
-class source_aperiodique: source {
+class source_aperiodique: public source {
     
     protected:
         float duree;
 };
 
-class source_sin: source_periodique {
+class source_sin: public source_periodique {
     public:
         source_sin(float t, float a, float p);
-        float ve(float t);
+        float ve(float t){
+            return amplitude*sin(2*M_PI*t/periode + phase);
+        };
     
 };
 
-class source_triangulaire : source_periodique{
+class source_triangulaire : public source_periodique{
     public:
         source_triangulaire(float t, float a, float p);
         float ve(float t);
 };
 
-class creneau : source_periodique{
+class creneau : public source_periodique{
     public:
         creneau(float t, float a, float p);
         float ve(float t);
@@ -50,7 +52,7 @@ class creneau : source_periodique{
         float largeur;
 };
 
-class source_rectangulaire: source_aperiodique{
+class source_rectangulaire: public source_aperiodique{
     public:
         source_rectangulaire(float t, float a, float p);
         float ve(float t);
@@ -58,8 +60,15 @@ class source_rectangulaire: source_aperiodique{
         float rapport_cyclique;
 };
 
-class echelon: source_aperiodique{
+class echelon: public source_aperiodique{
     public:
         echelon(float t, float a, float p);
-        float ve(float t);
+        float ve(float t){
+            if(t > phase){
+                return amplitude;
+            }
+            else{
+                return 0;
+            }
+        };
 };
